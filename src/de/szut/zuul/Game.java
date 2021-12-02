1f19b72cb0b4c1afdc24ec24e2975907166311c0
Justares <j.elmadhoun@gmail.com>
2021-12-02 22:14:15 +0100

    zuul4

---
+++ src/de/szut/zuul/Game.java
@@ -0,0 +1,200 @@
+package de.szut.zuul;
+
+/**
+ * This class is the main class of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.  Users
+ * can walk around some scenery. That's all. It should really be extended
+ * to make it more interesting!
+ * <p>
+ * To play this game, create an instance of this class and call the "play"
+ * method.
+ * <p>
+ * This main class creates and initialises all the others: it creates all
+ * rooms, creates the parser and starts the game.  It also evaluates and
+ * executes the commands that the parser returns.
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+
+public class Game {
+    private Parser parser;
+    private Room currentRoom;
+
+    /**
+     * Create the game and initialise its internal map.
+     */
+    public Game() {
+        createRooms();
+        parser = new Parser();
+    }
+
+    /**
+     * Create all the rooms and link their exits together.
+     */
+    private void createRooms() {
+        Room marketsquare, templePyramid, tavern, sacrificialSite, hut, jungle, secretPassage, cave, beach, roomOfMage, basement;
+
+        // create the rooms
+        marketsquare = new Room("on the market square");
+        templePyramid = new Room("in a temple pyramid");
+        tavern = new Room("in the tavern at the market square");
+        sacrificialSite = new Room("at a sacrificial site");
+        hut = new Room("in a hut");
+        jungle = new Room("in the jungle");
+        secretPassage = new Room("in a secret passage");
+        cave = new Room("in a cave");
+        beach = new Room("on the beach");
+        roomOfMage = new Room("in the room of the mage");
+        basement = new Room("in a dark basement");
+
+        // initialise room exits
+        marketsquare.setExit("west", sacrificialSite);
+        marketsquare.setExit("north", tavern);
+        marketsquare.setExit("east", templePyramid);
+        templePyramid.setExit("west", marketsquare);
+        templePyramid.setExit("north", hut);
+        templePyramid.setExit("up", roomOfMage);
+        templePyramid.setExit("down", basement);
+        tavern.setExit("south", marketsquare);
+        tavern.setExit("east", hut);
+        sacrificialSite.setExit("east", marketsquare);
+        sacrificialSite.setExit("down", cave);
+        hut.setExit("west", tavern);
+        hut.setExit("south", templePyramid);
+        hut.setExit("east", jungle);
+        jungle.setExit("west", hut);
+        secretPassage.setExit("west", cave);
+        secretPassage.setExit("east", basement);
+        cave.setExit("up", sacrificialSite);
+        cave.setExit("east", secretPassage);
+        cave.setExit("south", beach);
+        beach.setExit("north",cave);
+        roomOfMage.setExit("down",templePyramid);
+        basement.setExit("up",templePyramid);
+        basement.setExit("west", secretPassage);
+
+
+
+        currentRoom = marketsquare;  // start game on marketsquare
+    }
+
+    /**
+     * Main play routine.  Loops until end of play.
+     */
+    public void play() {
+        printWelcome();
+
+        // Enter the main command loop.  Here we repeatedly read commands and
+        // execute them until the game is over.
+
+        boolean finished = false;
+        while (!finished) {
+            Command command = parser.getCommand();
+            finished = processCommand(command);
+        }
+        System.out.println("Thank you for playing.  Good bye.");
+    }
+
+    private void printRaumInfo() {
+
+        System.out.println();
+        System.out.println("You are " + currentRoom.getDescription());
+        System.out.print("Exits: ");
+        System.out.println(currentRoom.exitsToString());
+
+    }
+
+    /**
+     * Print out the opening message for the player.
+     */
+    private void printWelcome() {
+        System.out.println();
+        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Type 'help' if you need help.");
+        printRaumInfo();
+    }
+
+    /**
+     * Given a command, process (that is: execute) the command.
+     *
+     * @param command The command to be processed.
+     * @return true If the command ends the game, false otherwise.
+     */
+    private boolean processCommand(Command command) {
+        boolean wantToQuit = false;
+
+        if (command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String commandWord = command.getCommandWord();
+        if (commandWord.equals("help")) {
+            printHelp();
+        } else if (commandWord.equals("go")) {
+            goRoom(command);
+        } else if (commandWord.equals("quit")) {
+            wantToQuit = quit(command);
+        }
+
+        return wantToQuit;
+    }
+
+    // implementations of user commands:
+
+    /**
+     * Print out some help information.
+     * Here we print some stupid, cryptic message and a list of the
+     * command words.
+     */
+    private void printHelp() {
+        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("through the jungle. At once there is a glade. On it there a buildings...");
+        System.out.println();
+        System.out.println("Your command words are:");
+        System.out.println("   go quit help");
+    }
+
+    /**
+     * Try to go in one direction. If there is an exit, enter
+     * the new room, otherwise print an error message.
+     */
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // Try to leave current room.
+        Room nextRoom = null;
+        nextRoom = currentRoom.getExit(direction);
+
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        } else {
+            currentRoom = nextRoom;
+            printRaumInfo();
+        }
+    }
+
+    /**
+     * "Quit" was entered. Check the rest of the command to see
+     * whether we really quit the game.
+     *
+     * @return true, if this command quits the game, false otherwise.
+     */
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
+            System.out.println("Quit what?");
+            return false;
+        } else {
+            return true;  // signal that we want to quit
+        }
+    }
+}
---
+++ src/de/szut/zuul/Room.java
@@ -0,0 +1,68 @@
+package de.szut.zuul;
+
+import java.util.HashMap;
+
+/**
+ * Class Room - a room in an adventure game.
+ * <p>
+ * This class is part of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.
+ * <p>
+ * A "Room" represents one location in the scenery of the game.  It is
+ * connected to other rooms via exits.  The exits are labelled north,
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+public class Room {
+    private String description;
+    HashMap<String, Room> roomExit;
+
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     *
+     * @param description The room's description.
+     */
+    public Room(String description) {
+        this.description = description;
+         roomExit = new HashMap<String, Room>();
+    }
+
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     *
+     */
+    public void setExit(String direction, Room neighbour) {
+        this.roomExit.put(direction,neighbour);
+    }
+
+
+    public Room getExit(String direction) {
+
+        return roomExit.get(direction);
+    }
+
+    public String exitsToString() {
+        String str="";
+        for(String direction : roomExit.keySet()) {
+            str+= direction + " ";
+        }
+        return str;
+    }
+
+
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+}
